95d81a216ae5c18e331fac898a6b6f21339abea7
Alexey <leskiy@yandex.ru>
2025-05-16 14:09:34 +0300

    homework sprint 15 test testcontainers

---
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -0,0 +1,33 @@
+package ru.practicum.shareit;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.DockerClientFactory;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@SpringBootTest
+class ShareItTests {
+
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16.1");
+
+    @BeforeAll
+    static void beforeAll(){
+        DockerClientFactory.instance().client();
+        postgres.start();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Test
+    void contextLoads() {
+    }
+
+}
