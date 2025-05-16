696c231f2ccb8b42a208b787df33a46fd4d4d2d4
Alexey <leskiy@yandex.ru>
2025-05-16 14:13:22 +0300

    homework sprint 15 test testcontainers checkstyle fixes

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.DockerClientFactory;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest
@@ -14,8 +13,7 @@ class ShareItTests {
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16.1");
 
     @BeforeAll
-    static void beforeAll(){
-        DockerClientFactory.instance().client();
+    static void beforeAll() {
         postgres.start();
     }
 
